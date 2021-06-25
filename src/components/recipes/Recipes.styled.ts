6d07b202f38607a4e7c822efc94ecd0079fa8902
nemanjagajic <nemanja.gajicru96@gmail.com>
2021-06-25 19:24:36 +0200

    recipes styled

--- src/components/recipes/Recipes.styled.ts
+++ src/components/recipes/Recipes.styled.ts
@@ -10,23 +10,45 @@ export const Wrapper = styled.div`
 export const RecipesWrapper = styled.div`
   width: 600px;
   margin-top: 40px;
+  cursor: pointer;
 `
 
 export const RecipeItem = styled.div`
   margin-bottom: 40px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.gray_100};
-  padding: 30px;
   border-radius: 20px;
+  height: 400px;
+  border: ${({ theme }) => `1px solid ${theme.gray_400}`};
 `
 
 export const Image = styled.img`
-  width: 100px;
-  height: 100px;
+  width: 100%;
+  height: 60%;
   margin-bottom: 20px;
+  object-fit: cover;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`
+
+export const ImagePlaceholder = styled.div`
+  width: 100%;
+  height: 60%;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  background-color: ${({ theme }) => theme.gray_400};
+  color: ${({ theme }) => theme.gray_500};
+`
+
+export const NoImageText = styled.div`
+  margin-top: 10px;
 `
 
 export const TitleWrapper = styled.div`
--- src/components/recipes/Recipes.tsx
+++ src/components/recipes/Recipes.tsx
@@ -1,10 +1,12 @@
 import React, {useEffect} from 'react';
 import { useFetchRecipes } from '../../hooks/recipes/useFetchRecipes'
-import imagePlaceholder from '../../assets/recipePlaceholder.png'
 import * as Styled from './Recipes.styled'
 import { Title, Text } from '../../styles/shared'
 import { IMAGES_LOCATION } from '../../constants/constants'
 import {useFetchCategories} from '../../hooks/categories/useFetchCategories'
+import $t from '../../i18n'
+import { Image } from 'react-ionicons'
+import theme from '../../theme/theme'
 
 const TITLE_MARGIN_BOTTOM = 20
 
@@ -21,9 +23,10 @@ const Recipes = ({ categoryId, setNavbarTitle }: PropTypes) => {
     if (setNavbarTitle) setNavbarTitle(getCategoryTitle())
   }, [categories])
 
-  const renderImages = (images: string[]) => {
-    return images.map((image, index) => <Styled.Image key={index} src={`${IMAGES_LOCATION}${image}`}/>)
-  }
+  // TODO move this into recipe details page
+  // const renderImages = (images: string[]) => {
+  //   return images.map((image, index) => <Styled.Image key={index} src={`${IMAGES_LOCATION}${image}`}/>)
+  // }
 
   const renderCoverImage = (coverImage: string) => {
     return (
@@ -41,8 +44,12 @@ const Recipes = ({ categoryId, setNavbarTitle }: PropTypes) => {
       <Styled.RecipesWrapper>
         {!isFetchingRecipes && recipes?.map((recipe) => (
           <Styled.RecipeItem key={recipe._id}>
-            {recipe.coverImage ? renderCoverImage(recipe.coverImage) : <Styled.Image src={imagePlaceholder}/>}
-            {renderImages(recipe.images)}
+            {recipe.coverImage ? renderCoverImage(recipe.coverImage) : (
+              <Styled.ImagePlaceholder>
+                <Image width={'60px'} height={'60px'} color={theme.gray_500} />
+                <Styled.NoImageText>{$t('recipes.noCoverImageAdded')}</Styled.NoImageText>
+              </Styled.ImagePlaceholder>
+            )}
             <Title marginBottom={TITLE_MARGIN_BOTTOM}>{recipe.title}</Title>
             <Text>{recipe.shortDescription}</Text>
           </Styled.RecipeItem>
