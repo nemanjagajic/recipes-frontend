2d6eb9b36a4d2b29c4485dc6adcd45b0c3f66304
nemanjagajic <nemanja.gajicru96@gmail.com>
2021-07-16 18:59:36 +0200

    added serbian translation

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,16 +2,20 @@ import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 // @ts-ignore
-import en from './locale/en'
+import en from './locale/en.json'
+import rs from './locale/rs.json'
 
 i18n.use(LanguageDetector).init({
+  lng: 'rs',
   resources: {
+    rs: {
+      translations: rs
+    },
     en: {
       translations: en
     }
   },
-
-  fallbackLng: 'en',
+  fallbackLng: 'rs',
   ns: ['translations'],
   defaultNS: 'translations'
 });
